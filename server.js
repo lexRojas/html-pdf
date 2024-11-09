fe57c74da2dde7c7ae1286349495f19b06080057
lexrojas <legalcr.rojas@gmail.com>
2024-11-09 14:56:28 -0600

    eeee

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { chromium } = require('playwright');
+
+const app = express();
+const PORT = 3001; // Puedes cambiar el puerto si es necesario
+
+app.use(express.json());
+
+app.post('/generate-pdf', async (req, res) => {
+    const { htmlContent } = req.body;
+
+    try {
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
+        await page.setContent(htmlContent, { waitUntil: 'domcontentloaded' });
+        const pdfBuffer = await page.pdf({ format: 'A4' });
+
+        await browser.close();
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(pdfBuffer);
+    } catch (error) {
+        console.error('Error generating PDF:', error);
+        res.status(500).send('Failed to generate PDF');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
